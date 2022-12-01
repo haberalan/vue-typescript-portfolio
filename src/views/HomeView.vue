c51daf0f92e521e533ace47c0f7a8934e045815d
Alan <alanhaberapp@pm.me>
2022-12-01 17:52:45 +0100

    style changes

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,11 +3,11 @@
     <v-container class="my-auto">
       <v-row class="d-flex align-center">
         <v-spacer />
-        <v-col cols="12" class="mb-16 v-col-lg-4 text-center text-lg-left">
+        <v-col cols="12" class="mb-16 v-col-sm-6 v-col-xl-4 text-center text-lg-left">
           <h1 class="text-h4 text-sm-h2">I'm <strong>Alan Haber</strong></h1>
           <p class="text-body-1 text-sm-h5 mt-2 text-grey-darken-2">Junior Frontend Developer</p>
         </v-col>
-        <v-col cols="12" class="v-col-lg-4">
+        <v-col cols="12" class="v-col-xl-4 v-col-sm-6">
           <v-sheet class="mx-auto" max-width="400">
             <vue-3-lottie :animationData="homeJSON" />
           </v-sheet>
